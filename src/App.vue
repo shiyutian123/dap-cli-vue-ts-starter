bbabe90e3f9a91a112f19d5388c6dce708c70eea
BeyondJSC <2511907843@qq.com>
2020-10-27 15:53:36 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,25 @@
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import { State, Mutation, namespace } from 'vuex-class'
+const authModule = namespace('authModule')
+
+@Component
+export default class BasicLayout extends Vue {
+  @authModule.Getter('tokenValue') tokenValue: any;
+
+  created() {
+    if (this.tokenValue) {
+      (Vue as any).addHeaders({
+        AuthorizationToken: this.tokenValue
+      });
+    }
+  }
+}
+</script>
+
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
