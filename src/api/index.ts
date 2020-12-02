97835fcaf922224cf772693372934daeab61a12d
BeyondJSC <2511907843@qq.com>
2020-12-02 09:54:27 +0800

    添加用户信息

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,5 +3,7 @@ import ProcessActionApi from './processAction'
 export default {
   LOCAL_LOGIN: '/fnd/user/login',
   QUERY_APP_INFO: '/app/queryAppInfo',
+  // 获取登录用户信息
+  QUERY_LOGIN_USER_INFO: '/fnd/user/loginInfo' 
   // 流程审批相关
 }
